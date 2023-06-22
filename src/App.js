ce5035b90540d01f97b61bb0448462b8d26daae1
miteshvasoya <mrvasoya2001@gmail.com>
2023-06-22 16:57:57 +0530

    Banner Div Setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import './App.css';
 import HomePage from "./page/Homepage";
 
 function App() {
-    return (
-        <div className="App">
-            <HomePage/>
-        </div>
-    );
+    return (<div className="App">
+        <HomePage/>
+    </div>);
 }
 
 export default App;
--- src/page/Homepage.jsx
+++ src/page/Homepage.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./HomePage.css"
+import Homepage_SpaceCenter_Image from "../assets/Homepage_SpaceCenter_Image.jpg"
+import SpaceImage from "../assets/Homepage_Space Image.png"
+import SpaceShuttle from "../assets/Space-Shuttle.png"
 
 //Homepage component
 class HomePage extends React.Component {
@@ -7,17 +10,75 @@ class HomePage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            // Initial state goes here
+            scrollHeight: 0, bgImg1Scale: 1.2, bgImg2Scale: 3, shuttleImgScale: 1,
         };
+        this.scrollRef = React.createRef();
+        this.initialBackgroundImg = "width: 100%;height: 100%;object-fit: cover;opacity: 0.7;position: absolute;transform: scale(120%)  translate(-8%, -8%);"
+    }
+
+    handleScroll = () => {
+        const banner = document.getElementById("banner-id");
+        const bgImg1 = document.getElementById("bg-img1-id");
+        const bgImg2 = document.getElementById("bg-img2-id");
+        const spaceShuttle = document.getElementById("space-shuttle-id");
+
+        if (window.scrollY === 0) {
+            let bottom = 1.99
+            // banner.style.opacity = 1
+            const bottomSetInterval = setInterval(() => {
+                if (bottom >= -2) {
+                    bottom -= 0.2
+                    spaceShuttle.style = `bottom :${bottom}vw`
+                } else {
+                    clearInterval(bottomSetInterval)
+                }
+            }, 30)
+
+        } else if (window.scrollY === 2 * window.innerHeight) {
+            bgImg2.style = `opacity :1`
+
+        } else {
+
+            const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+            const maxScroll = 500;
+            const maxScroll2 = 1000;
+            const minScale = 0.4;
+            const scrollRange = Math.min(scrollTop, maxScroll);
+            const scrollRange2 = Math.min(scrollTop, maxScroll2);
+            const bgImg1Scale = 1 + ((1.2 - 1) * (maxScroll - scrollRange) / maxScroll);
+            const bgImg2Scale = 1 + ((8 - 1) * (maxScroll2 - scrollRange2) / maxScroll);
+            const shuttleImgScale = 1 - (scrollRange / maxScroll) * (1 - minScale) / 2;
+            this.setState({bgImg1Scale});
+            this.setState({shuttleImgScale});
+            bgImg1.style = `transform : scale(${bgImg1Scale})`;
+            spaceShuttle.style = `transform : scale(${shuttleImgScale}); bottom : ${shuttleImgScale * 2}vw`
+            banner.style.opacity = Math.abs(1 - (scrollTop) / 800);
+
+            console.log("Scrolling...", window.innerHeight);
+            // bgImg2.style.opacity = Math.abs(1 - (1 - (scrollTop) / 800));
+
+            bgImg2.style = `transform : scale(${bgImg2Scale}); opacity : ${Math.abs(1 - (1.5 - (scrollTop) / 800))}`
+        }
+    };
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
     }
 
     render() {
+        const {scrollHeight} = this.state;
         return (<>
-            <div className="homepage">
-                <div className="banner">
-                    <img src="" alt=""/>
-                    <p>Welcome To</p>
-                    <p>SpaceX</p>
+            <div className="homepage" ref={this.scrollRef} onScroll={this.handleScroll}>
+                <div className="banner" id="banner-id">
+                    <img src={Homepage_SpaceCenter_Image} alt="Base Space Station Image" id="bg-img1-id"/>
+                    <p id="welcome-title-id">Welcome To</p>
+                    <p id="spacex-title-id">SpaceX</p>
+                    <img src={SpaceShuttle} alt="Space-Shuttle" id="space-shuttle-id"
+                    />
                 </div>
             </div>
         </>);
